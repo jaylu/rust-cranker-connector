20b407fc43fa736082026a9e8d914840bc14b05f
jaylu <jayjjlu@gmail.com>
2022-03-12 23:16:08 +0800

    update dependency version

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,24 @@
-use async_tungstenite::{async_std::connect_async, tungstenite::Error, tungstenite::Result, WebSocketStream};
-use async_tungstenite::async_std;
-use async_tungstenite::async_std::ConnectStream;
-use async_tungstenite::tungstenite::handshake::client::Response;
+use std::env;
+
+use async_std::io;
+use async_std::prelude::*;
+use async_std::task;
+use async_tungstenite::async_std::connect_async;
+use async_tungstenite::tungstenite::protocol::Message;
 
 async fn connect_to_router() {
     let result = connect_async("wss://localhost:16488/register?connectorId=abc").await;
     match result {
-        Ok((stream, response)) => handle_wss_connection(stream, response),
-        Err(e) => handle_wss_connection_error(e),
+        Ok((stream, response)) => {
+            let (write, read) = stream.split();
+            read.for_each(|message| async {
+                let data = message.unwrap().into_data();
+            });
+        },
+        Err(e) => {
+            todo!()
+        },
     }
-}
-
-fn handle_wss_connection_error(error: Error) -> () {
-    todo!()
-}
-
-fn handle_wss_connection(stream: WebSocketStream<ConnectStream>, response: Response) -> () {
-    let (write, read) = stream.split();
-    read.for_each(|message| async {
-        let data = message.unwrap().into_data();
-        tokio::io::stdout().write_all(&data).await.unwrap();
-    });
 }
 
 #[tokio::main]
