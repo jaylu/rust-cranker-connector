c476549f3e13b851aeaef0d8f5f8b349f2b518b7
jaylu <jayjjlu@gmail.com>
2022-05-03 21:36:27 +0800

    get and post both working now

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use std::borrow::Borrow;
 use std::str::FromStr;
+use std::sync::atomic::{AtomicUsize, Ordering};
 
 use async_native_tls::TlsConnector;
-use async_tungstenite::async_std::{connect_async_with_tls_connector};
+use async_tungstenite::async_std::connect_async_with_tls_connector;
 use async_tungstenite::tungstenite::handshake::client::generate_key;
 use async_tungstenite::tungstenite::http::Request;
 use async_tungstenite::tungstenite::http::request::Builder;
@@ -9,15 +11,56 @@ use async_tungstenite::tungstenite::Message;
 use async_tungstenite::tungstenite::protocol::CloseFrame;
 use async_tungstenite::tungstenite::protocol::frame::coding::CloseCode;
 use bytes::Bytes;
-use futures::{SinkExt};
+use futures::SinkExt;
 use futures::StreamExt;
+use http::Uri;
 use hyper::{Body, Client, Method};
 use hyper::body::{HttpBody, Sender};
-use hyper::client::{ResponseFuture};
+use hyper::client::ResponseFuture;
 use hyper::Request as hyper_request;
 
-fn parse(input: &str) -> Builder {
+struct Config<> {
+    route: String,
+    target: String,
+    component: String,
+    uris_provider: fn() -> Vec<Uri>,
+    sliding_window: i8,
+}
 
+struct Connector {
+    config: Config,
+    idle_count: AtomicUsize,
+}
+
+impl Connector {
+    pub fn new(config: Config) -> Connector {
+        return Connector {
+            config,
+            idle_count: AtomicUsize::new(0),
+        };
+    }
+
+    pub async fn start(&mut self) -> &mut Connector {
+        let Config {
+            route,
+            target,
+            component,
+            uris_provider,
+            sliding_window
+        } = &self.config;
+
+
+
+        connect_to_router(target, &self.idle_count).await;
+        return self;
+    }
+
+    pub fn stop(&mut self) -> &mut Connector {
+        return self;
+    }
+}
+
+fn parse(input: &str, target: &str) -> Builder {
     let lines: Vec<&str> = input.split("\n").collect();
     let first_line_fields: Vec<&str> = lines[0].split(" ").collect();
     let method = first_line_fields[0];
@@ -33,12 +76,14 @@ fn parse(input: &str) -> Builder {
         }
     }
 
+    let uri = format!("{}{}", target, path);
+    println!("uri={}", uri);
     return request_builder
         .method(Method::from_str(method).unwrap())
-        .uri(format!("{}{}", "http://localhost:8080", path));
+        .uri(uri);
 }
 
-async fn connect_to_router() {
+async fn connect_to_router(target: &str, idle_count: &mut AtomicUsize ) {
     let client = Client::new();
     let connector = TlsConnector::new().danger_accept_invalid_certs(true);
     let request = Request::builder()
@@ -54,54 +99,57 @@ async fn connect_to_router() {
         .body(())
         .unwrap();
 
-    let result = connect_async_with_tls_connector(request, Some(connector)).await;
-    match result {
+    match connect_async_with_tls_connector(request, Some(connector)).await {
         Ok((stream, _response)) => {
             let (mut write, mut read) = stream.split();
 
             let mut body_writer: Option<Sender> = None;
             let mut client_request_future: Option<ResponseFuture> = None;
+            let mut is_consumed = false;
 
             while let Some(message) = read.next().await {
+                if !is_consumed {
+                    is_consumed = true;
+                    // idle_count.fetch_sub(1, Ordering::SeqCst);
+                }
+
                 match message {
                     Ok(msg) => {
                         if msg.is_text() {
-                            // construct http reqeust
+                            // construct http request
                             let text_line = msg.into_text().unwrap();
                             println!("text: {:?}", text_line);
                             if text_line.ends_with("_1") {
 
                                 // has body
-
-                                let (sender , body) = Body::channel();
+                                let (sender, body) = Body::channel();
                                 body_writer = Some(sender);
 
-                                let request_builder = parse(&text_line);
+                                let request_builder = parse(&text_line, target);
                                 let req = request_builder.body(body).unwrap();
 
                                 client_request_future = Some(client.request(req));
-
                             } else if text_line.ends_with("_2") {
 
                                 // no body
 
-                                let req = parse(&text_line).body(Body::empty()).unwrap();
+                                let req = parse(&text_line, target).body(Body::empty()).unwrap();
                                 let mut resp = client.request(req).await.unwrap();
 
                                 match write.send(Message::Text(format!("HTTP/1.1 {} OK\n", resp.status()))).await {
-                                    Ok(_) => { println!("tx sent text") }
-                                    Err(_) => { println!("tx sent text error") }
+                                    Ok(_) => { println!("tx response sent text") }
+                                    Err(_) => { println!("tx response sent text error") }
                                 }
 
                                 while let Some(next) = resp.data().await {
                                     match next {
                                         Ok(chunk) => {
                                             match write.send(Message::Binary(chunk.to_vec())).await {
-                                                Ok(_) => { println!("tx sent binary"); }
-                                                Err(_) => { println!("tx sent binary error") }
+                                                Ok(_) => { println!("tx response sent binary"); }
+                                                Err(_) => { println!("tx response sent binary error") }
                                             }
                                         }
-                                        Err(err) => {println!("error {}", err) }
+                                        Err(err) => { println!("error {}", err) }
                                     }
                                 }
 
@@ -109,11 +157,10 @@ async fn connect_to_router() {
                                     Message::Close(Some(CloseFrame { code: CloseCode::Normal, reason: "normal close".into() }))
                                 ).await {
                                     Ok(_) => { println!("close done"); }
-                                    Err(_) => { println!("tx sent binary error") }
+                                    Err(_) => { println!("close error") }
                                 }
 
                                 println!("write close")
-
                             } else if text_line.ends_with("_3") {
 
                                 // body send completed
@@ -134,7 +181,9 @@ async fn connect_to_router() {
                                                 Err(_) => { println!("tx sent binary error") }
                                             }
                                         }
-                                        Err(err) => {println!("error {}", err) }
+                                        Err(err) => {
+                                            println!("error {}", err)
+                                        }
                                     }
                                 }
 
@@ -146,18 +195,15 @@ async fn connect_to_router() {
                                 }
 
                                 println!("write close")
-
                             } else {}
                         } else if msg.is_binary() {
-
                             if body_writer.as_mut().is_some() {
                                 let writer = body_writer.as_mut().unwrap();
                                 match writer.send_data(Bytes::from(msg.into_data())).await {
-                                    Ok(_) => {println!("writer send data done")},
-                                    Err(_) => {println!("writer send data err")},
+                                    Ok(_) => { println!("writer send data done") }
+                                    Err(_) => { println!("writer send data err") }
                                 };
                             }
-
                         } else if msg.is_close() {
                             // clean up
                         }
@@ -174,18 +220,30 @@ async fn connect_to_router() {
 
 #[tokio::main]
 async fn main() {
-    connect_to_router().await
+    let config = Config {
+        route: String::from("*"),
+        target: String::from("http://localhost:8080"),
+        component: String::from("rust-testing-component"),
+        uris_provider: || { vec![Uri::from_str("wss://localhost:16488/register?connectorId=abc").unwrap()] },
+        sliding_window: 2,
+    };
+    let mut connector = Connector::new(config);
+    connector.start().await;
     // test_main().await
 }
 
 #[cfg(test)]
 mod tests {
+    use std::str::FromStr;
+
+    use http::Uri;
+
     use crate::parse;
 
     #[test]
     fn test_parse() {
         let input = "POST /post-msg HTTP/1.1\nUser-Agent:curl/7.64.1\nHost:localhost:9443\nAccept:*/*\nContent-Length:52\nContent-Type:application/json\nForwarded:for=0:0:0:0:0:0:0:1;proto=https;host=localhost:9443;by=0:0:0:0:0:0:0:1\nX-Forwarded-For:0:0:0:0:0:0:0:1\nX-Forwarded-Proto:https\nX-Forwarded-Host:localhost:9443\nX-Forwarded-Server:0:0:0:0:0:0:0:1\n\n_1";
-        let builder = parse(input);
+        let builder = parse(input, "http://localhost:8080");
         let headers = builder.headers_ref().unwrap();
         assert_eq!(headers["X-Forwarded-Server"], "0:0:0:0:0:0:0:1");
         assert_eq!(headers["Accept"], "*/*");
